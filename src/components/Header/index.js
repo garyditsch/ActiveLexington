4c7ac8eabc04d7d162dfc8ab908ad95233aa3007
Gary Ditsch <garyditsch@gmail.com>
2018-01-29 16:11:54 -0500

    adding materialui to project and dropdown to header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import IconButton from 'material-ui/IconButton';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 
 const Header = (props) => (
     <header>
@@ -10,9 +14,27 @@ const Header = (props) => (
                         <Link to="/" className="navbar-brand">
                             {props.metaData.title}
                         </Link>
-                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
+                        <button className="navbar-toggler" type="button">
+                            <IconMenu
+                              iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
+                              anchorOrigin={{horizontal: 'left', vertical: 'top'}}
+                              targetOrigin={{horizontal: 'left', vertical: 'top'}}
+                            >
+                              <MenuItem
+                                  primaryText="Home"
+                                  containerElement={<Link to="/" />}
+                              />
+                              <MenuItem
+                                  primaryText="Highlights"
+                                  containerElement={<Link to="/#highlights" />}
+                              />
+                              <MenuItem
+                                  primaryText="Listen to Podcast"
+                                  containerElement={<Link to="/#episodes" />}
+                              />
+
+                            </IconMenu>
+                        </button>
                         <div className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
                             <ul className="navbar-nav">
                                 <li className="nav-item">
